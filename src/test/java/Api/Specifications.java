7bd1f3aa74a61b07e4d795355d49fe3c27d152c2
Guardian <antonzelenkov112@gmail.com>
2023-11-05 15:10:49 +0300

    Version 2

---
+++ src/test/java/Api/Specifications.java
@@ -0,0 +1,37 @@
+package Api;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class Specifications {
+    public static RequestSpecification recrestSpec(String url){
+        return new RequestSpecBuilder()
+                .setBaseUri(url)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+    public static ResponseSpecification responseSpecOK200(){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+    }
+    public static ResponseSpecification responseSpecOK400(){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(400)
+                .build();
+    }
+    public static void installSpecification(RequestSpecification requestSpec, ResponseSpecification responseSpec){
+        RestAssured.requestSpecification = requestSpec;
+        RestAssured.responseSpecification = responseSpec;
+    }
+    public static void installSpecification(RequestSpecification requestSpec){
+        RestAssured.requestSpecification =requestSpec;
+    }
+    public static void installSpecification(ResponseSpecification responseSpec){
+        RestAssured.responseSpecification =responseSpec;
+    }
+}
